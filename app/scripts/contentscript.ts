17b0e23bffff2cd3fa5e88782e42057830e3e5e2
nagaminenot <nagamine@efg.company>
2018-04-30 22:41:17 +0900

    DOMの確認ができるように

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,4 +1,14 @@
 // Enable chromereload by uncommenting this line:
-// import 'chromereload/devonly'
+import 'chromereload/devonly'
 
-console.log(`'Allo 'Allo! Content script`);
+document.addEventListener('click', function (e) {
+    // e.target: イベント発生源
+    if (e.path[0].innerText) {
+        console.log('=>' + String(e.path[0].innerText))
+    } else if (e.path[0].value) {
+        console.log('=>' + String(e.path[0].value))
+    } else if (e.path[0].alt) {
+        console.log('=>' + String(e.path[0].alt))
+    } else {
+    }
+}, false);
