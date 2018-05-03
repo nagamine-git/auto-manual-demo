45f51df859248a772957b8a49b5ad6798ab07d76
Tsuyoshi Nagamine <nagamine@mba.local>
2018-05-03 12:20:40 +0900

    URL取得

--- app/scripts/contentscript.ts
+++ app/scripts/contentscript.ts
@@ -1,8 +1,15 @@
 // Enable chromereload by uncommenting this line:
 import 'chromereload/devonly'
 
+let last_location = '';
+
 document.addEventListener('click', (e) => {
     // e.target: イベント発生源
+    let current_location = location.href;
+    if (last_location !== location.href) {
+      console.log(location.href);
+      last_location = location.href;
+    }
     if (e.path[0].innerText) {
         console.log('=>' + String(e.path[0].innerText))
     } else if (e.path[0].value) {
